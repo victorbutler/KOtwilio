74a2214af68796a5523c86cb8bfd443b062f0968
Victor Butler <victorbutler@gmail.com>
2012-01-06 19:24:35 -0500

    initial commit

---
+++ classes/kohana/kotwilio.php
@@ -0,0 +1,48 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Twilio for Kohana.
+ *
+ * @package    Kotwilio
+ * @author     Victor Butler <victorbutler@gmail.com>
+ * @copyright  (c) 2012 Victor Butler
+ * @license    MIT
+ */
+abstract class Kohana_Kotwilio {
+
+	protected static $_instance;
+
+	/**
+	 * Singleton pattern
+	 *
+	 * @return Kotwilio
+	 */
+	public static function instance() {
+		if (!isset(Kotwilio::$_instance)) {
+			Kotwilio::$_instance = new Kotwilio();
+		}
+		return Kotwilio::$_instance;
+	}
+
+	protected $_config;
+
+	protected $_twilio;
+
+	/**
+	 *
+	 *
+	 */
+	public function __construct($config = array()) {
+		// Get our Twilio identification
+		$this->_config = (count($config) ? $config : Kohana::$config->load('kotwilio'));
+		$this->_twilio = new Services_Twilio($this->_config['account_sid'], $this->_config['auth_token']);
+	}
+
+	/**
+	 *
+	 *
+	 */
+	public function sms($from, $to, $body) {
+		return $this->_twilio->account->sms_messages->create($from, $to, $body);
+	}
+
+}
---
+++ classes/kotwilio.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Kotwilio extends Kohana_Kotwilio {  }
